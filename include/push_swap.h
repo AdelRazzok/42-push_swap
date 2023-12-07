26187340555ec225fab263372f8fd279a45d6329
delin0 <lhp6.arazzok@gmail.com>
2023-12-07 18:02:36 +0100

    add: header and main files

---
+++ include/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/07 17:40:00 by arazzok           #+#    #+#             */
+/*   Updated: 2023/12/07 18:01:46 by arazzok          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <unistd.h>
+
+#endif
