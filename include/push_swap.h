6e998d20227673da58caffb693e699f8703c49d1
delin0 <lhp6.arazzok@gmail.com>
2023-12-07 22:41:06 +0100

    add: stack struct initialized

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:40:00 by arazzok           #+#    #+#             */
-/*   Updated: 2023/12/07 18:01:46 by arazzok          ###   ########.fr       */
+/*   Updated: 2023/12/07 22:32:14 by arazzok          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,23 @@
 
 # define PUSH_SWAP_H
 
-# include <stdlib.h>
+# include "../libft/include/libft.h"
 # include <limits.h>
 # include <stdbool.h>
+# include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_stack_node
+{
+	int					value;
+	int					current_position;
+	int					final_index;
+	int					push_price;
+	bool				above_median;
+	bool				cheapset;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,11 +6,20 @@
 /*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 17:59:06 by arazzok           #+#    #+#             */
-/*   Updated: 2023/12/07 17:59:45 by arazzok          ###   ########.fr       */
+/*   Updated: 2023/12/07 22:40:22 by arazzok          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(int argc, char** argv)
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
 {
-    
+	t_stack_node	*a;
+	t_stack_node	*b;
+	
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	return (0);
 }
