edfe9db55dbc732ba8ec7d70f5ed23cdc6718fcf
delin0 <lhp6.arazzok@gmail.com>
2023-12-12 02:45:31 +0100

    fix: norm

--- src/error_handler.c
+++ src/error_handler.c
@@ -6,7 +6,7 @@
 /*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 01:52:18 by arazzok           #+#    #+#             */
-/*   Updated: 2023/12/12 02:24:50 by arazzok          ###   ########.fr       */
+/*   Updated: 2023/12/12 02:45:14 by arazzok          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,48 +18,48 @@ int	error_syntax(char *str)
 		return (1);
 	if ((*str == '+' || *str == '-') && !ft_isdigit(str[1]))
 		return (1);
-    while (*++str)
-    {
-        if (!ft_isdigit(*str))
-            return (1);
-    }
-    return (0);
+	while (*++str)
+	{
+		if (!ft_isdigit(*str))
+			return (1);
+	}
+	return (0);
 }
 
-int error_duplicate(t_stack_node *a, int n)
+int	error_duplicate(t_stack_node *a, int n)
 {
-    if (!a)
-        return (0);
-    while (a)
-    {
-        if (a->value == n)
-            return (1);
-        a = a->next;
-    }
-    return (0);
+	if (!a)
+		return (0);
+	while (a)
+	{
+		if (a->value == n)
+			return (1);
+		a = a->next;
+	}
+	return (0);
 }
 
-void    free_stack(t_stack_node **stack)
+void	free_stack(t_stack_node **stack)
 {
-    t_stack_node    *tmp;
-    t_stack_node    *current;
+	t_stack_node	*tmp;
+	t_stack_node	*current;
 
-    if (!stack)
-        return ;
-    current = *stack;
-    while (current)
-    {
-        tmp = current->next;
-        current->value = 0;
-        free(current);
-        current = tmp;
-    }
-    *stack = NULL;
+	if (!stack)
+		return ;
+	current = *stack;
+	while (current)
+	{
+		tmp = current->next;
+		current->value = 0;
+		free(current);
+		current = tmp;
+	}
+	*stack = NULL;
 }
 
-void    free_errors(t_stack_node **a)
+void	free_errors(t_stack_node **a)
 {
-    free_stack(a);
-    ft_putstr_fd("Error\n", STDERR_FILENO);
-    exit(1);
+	free_stack(a);
+	ft_putstr_fd("Error\n", STDERR_FILENO);
+	exit(1);
 }
