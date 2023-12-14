2bd488c82f6795aba182df585b7a7add3f0fc01a
delin0 <lhp6.arazzok@gmail.com>
2023-12-14 17:33:30 +0100

    add: rev_rotate commands

---
+++ src/commands/rev_rotate.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rev_rotate.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arazzok <arazzok@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/13 21:29:44 by arazzok           #+#    #+#             */
+/*   Updated: 2023/12/14 17:30:54 by arazzok          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+static void	rev_rotate(t_stack_node **stack)
+{
+	t_stack_node	*last_node;
+
+	if (!(*stack) || !(*stack)->next)
+		return ;
+	last_node = find_last(*stack);
+	last_node->prev->next = NULL;
+	last_node->next = *stack;
+	last_node->prev = NULL;
+	*stack = last_node;
+	last_node->next->prev = last_node;
+}
+void	rra(t_stack_node **a, bool print)
+{
+	rev_rotate(a);
+	if (!print)
+		ft_printf("rra\n");
+}
+
+void	rrb(t_stack_node **b, bool print)
+{
+	rev_rotate(b);
+	if (!print)
+		ft_printf("rrb\n");
+}
+
+void	rrr(t_stack_node **a, t_stack_node **b, bool print)
+{
+	rev_rotate(a);
+	rev_rotate(b);
+	if (!print)
+		ft_printf("rrr\n");
+}
